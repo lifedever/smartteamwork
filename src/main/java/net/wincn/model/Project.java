6fcd156675c8dd9b3bf9ad6c3ab896aba19979de
gefangshuai <gefangshuai@163.com>
2014-03-20 21:18:38 +0800

    add project

---
+++ src/main/java/net/wincn/model/Project.java
@@ -0,0 +1,8 @@
+package net.wincn.model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class Project extends Model<Project> {
+	private static final long serialVersionUID = 2779991564135437327L;
+	public static final Project dao = new Project();
+}
