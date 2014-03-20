d5be80fede7d2ec19e8a881c2aa2def9f2cf5ead
gefangshuai <gefangshuai@163.com>
2014-03-20 22:52:45 +0800

    修改样式

--- src/main/java/net/wincn/controller/user/ProjectController.java
+++ src/main/java/net/wincn/controller/user/ProjectController.java
@@ -1,11 +1,15 @@
 package net.wincn.controller.user;
 
+import net.wincn.interceptor.SessionInterceptor;
+
+import com.jfinal.aop.Before;
 import com.jfinal.core.ActionKey;
 import com.jfinal.core.Controller;
 
 /**
  * Created by gefangshuai on 2014-03-20-0020.
  */
+@Before(SessionInterceptor.class)
 public class ProjectController extends Controller {
 
 	@ActionKey("add")
